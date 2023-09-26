5c3586dbefbc05a0e85b29e98e7e38136b458aa1
otyheno <henryotieno66@gmail.com>
2023-09-26 15:50:54 +0300

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const firstBook = {
+    author: 'James Clear.',
+    title: 'Atomic Habits',
+    img: './images/atomic_habits.jpg'
+}
+
+const secondBook = {
+    author: 'Rebecca Yarros.',
+    title: 'Iron Flames',
+    img: './images/iron_flame.jpg'
+}
+
+// const author = 'James Clear.';
+// const title = 'Atomic Habits';
+// const img = './images/atomic_habits.jpg';
+
+{/* <Book author={author} title={title} img={img}/> */}
+
+function BookList(props){
+    const {img, title, author } = props;
+    return (
+        <section className='booklist'>
+            <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+            <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
+        </section>
+    );
+}
+
+const Book = (props) => {    
+    return <article className='book'>
+        <img src={props.img}></img>
+        <h2>{props.title}</h2>
+        <h4 style={{ color:'#617D98', fontSize:'0.75reb', marginTop:'0.5rem'}}>{props.author.toUpperCase()}</h4>
+    </article>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList/>);
+
+//export default Greeting;
